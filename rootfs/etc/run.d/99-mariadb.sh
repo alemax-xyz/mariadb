fd404b2946acc70544f961ecd0808aee410510dd
Alexey Maksutov <alexey.maksutov@gmail.com>
2026-05-22 16:08:36 -0400

    updates

--- rootfs/etc/run.d/99-mariadb.sh
+++ rootfs/etc/run.d/99-mariadb.sh
@@ -1,13 +1,15 @@
-[ -f /var/lib/mysql/auto.cnf ] || suexec sudo -E -u "$PUSER" mariadbd --bootstrap --skip-grant-tables <<-EOF || exit $?
+[ -f /var/lib/mysql/multi-master.info ] || suexec sudo -E -u "$PUSER" mariadbd --bootstrap --skip-grant-tables <<-EOF || exit $?
 	CREATE DATABASE IF NOT EXISTS mysql;
 	USE mysql;
-	$(cat /usr/share/mariadb/mariadb_system_tables.sql)
-	$(cat /usr/share/mariadb/mariadb_system_tables_data.sql)
-	$(cat /usr/share/mariadb/mariadb_performance_tables.sql)
-	$(cat /usr/share/mariadb/mariadb_sys_schema.sql)
-	$(cat /usr/share/mariadb/maria_add_gis_sp_bootstrap.sql)
-	$(cat /usr/share/mariadb/fill_help_tables.sql)
-	$(cat /usr/share/mariadb/init.sql)
+	$(cat \
+		/usr/share/mariadb/mariadb_system_tables.sql \
+		/usr/share/mariadb/mariadb_system_tables_data.sql \
+		/usr/share/mariadb/mariadb_performance_tables.sql \
+		/usr/share/mariadb/mariadb_sys_schema.sql \
+		/usr/share/mariadb/maria_add_gis_sp_bootstrap.sql \
+		/usr/share/mariadb/fill_help_tables.sql \
+		/usr/share/mariadb/init.sql \
+	)
 EOF
 
 suexec sudo -E -u "$PUSER" mariadbd --skip-name-resolve &
